c43225d02e4f763d4497f7753b80f1c79d02edc5
slgphantom <slgphantom@gmail.com>
2017-02-08 18:01:27 +0800

    Update Example.java

--- telegram/Example.java
+++ telegram/Example.java
@@ -1,16 +1,30 @@
 import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.TelegramBotsAPi;
+import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
-
 public class Example{
-    public static void main(String args[]){
-        TelegramBotsApi tgBot = new TelegramBotsApi();
-        ApiContextInitializer.init();
+  /*
+   * 程序主入口 
+   */
+  public static void main(String args[]){
+    /*
+     * 創建一個新的telegramAPI 在內存裡面
+     */
+    TelegramBotsApi tgBot = new TelegramBotsApi();    
+    /*
+     * 初始化telegramAPI的所有函數
+     */
+    ApiContextInitializer.init();
+    /*
+     *try{}catch(Exception e) 是一組條件 代表嘗試 如果有程序錯誤 執行catch 如沒有 catch則不會執行
+     */
         try {
-            tgBot.registerBot(new TelegramBot());
-        } catch (TelegramApiExpection e) {
-            System.out.println("error");
+          /*
+           * 把創建出來的telegramAPI與機器人連接
+           */
+          tgBot.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+          System.out.println("error");
         }
-    }
+  }
 }
